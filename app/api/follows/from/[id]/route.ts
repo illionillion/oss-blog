b0ab8e0baac516e94aa338d5c90b8d17f82b2905
minusno0708 <naoyao1014@gmail.com>
2024-09-14 16:56:53 +0900

    feat: パスパラメータを取得

--- app/api/follows/from/[id]/route.ts
+++ app/api/follows/from/[id]/route.ts
@@ -1,11 +1,18 @@
+import type { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
 
 import { responseMessage } from "@/app/api/types/responseMessage"
 
-export function GET() {
+export function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const userId: number = Number(params.id)
+
   return NextResponse.json(
     {
       message: responseMessage.success.get,
+      userId,
     },
     { status: 200 },
   )
