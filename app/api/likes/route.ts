ac38877b0df59d44638154ebb4a96f6ca603b5b4
minusno0708 <naoyao1014@gmail.com>
2024-09-15 02:59:49 +0900

    feat: いいね削除機能を実装

--- app/api/likes/route.ts
+++ app/api/likes/route.ts
@@ -97,3 +97,75 @@ export async function POST(request: NextRequest) {
     { status: 201 },
   )
 }
+
+export async function DELETE(request: NextRequest) {
+  let body
+
+  try {
+    body = await request.json()
+  } catch (error) {
+    // リクエストボディが存在しない場合にエラーを返す
+    console.log(error)
+    return NextResponse.json(
+      { message: responseMessage.error.invalidRequest },
+      { status: 400 },
+    )
+  }
+
+  if (!body.userId || !body.articleUrl) {
+    return NextResponse.json(
+      { message: responseMessage.error.invalidRequest },
+      { status: 400 },
+    )
+  }
+
+  const article = await prisma.article.findFirst({
+    where: {
+      url: body.articleUrl,
+    },
+    select: {
+      id: true,
+    },
+  })
+
+  // 記事が存在しない場合
+  if (!article) {
+    return NextResponse.json(
+      { message: responseMessage.error.notFound },
+      { status: 404 },
+    )
+  }
+
+  const like: Likes = {
+    userId: body.userId,
+    articleId: article.id,
+  }
+
+  // 重複したデータが存在するか確認
+  const likeData = await prisma.like.findFirst({
+    where: like,
+    select: {
+      id: true,
+    },
+  })
+
+  if (!likeData) {
+    return NextResponse.json(
+      { message: responseMessage.error.notFound },
+      { status: 404 },
+    )
+  }
+
+  await prisma.like.delete({
+    where: {
+      id: likeData.id,
+    },
+  })
+
+  return NextResponse.json(
+    {
+      message: responseMessage.success.delete,
+    },
+    { status: 201 },
+  )
+}
