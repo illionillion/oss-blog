8b69e8465942907f431bf539e840091f30c160da
minusno0708 <naoyao1014@gmail.com>
2024-09-15 02:28:53 +0900

    feat: 記事情報の更新機能を実装

--- app/api/articles/[url]/route.ts
+++ app/api/articles/[url]/route.ts
@@ -34,11 +34,61 @@ export async function GET(
   )
 }
 
-export function PUT() {
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { url: string } },
+) {
+  const url: string = params.url
+
+  let body
+
+  try {
+    body = await request.json()
+  } catch (error) {
+    // リクエストボディが存在しない場合にエラーを返す
+    console.log(error)
+    return NextResponse.json(
+      { message: responseMessage.error.invalidRequest },
+      { status: 400 },
+    )
+  }
+
+  if (!body.url) {
+    return NextResponse.json(
+      { message: responseMessage.error.invalidRequest },
+      { status: 400 },
+    )
+  }
+
+  const article = await prisma.article.findFirst({
+    where: {
+      url: url,
+    },
+    select: {
+      id: true,
+    },
+  })
+
+  if (!article) {
+    return NextResponse.json(
+      { message: responseMessage.error.notFound },
+      { status: 404 },
+    )
+  }
+
+  const responseData = await prisma.article.update({
+    where: {
+      id: article.id,
+    },
+    data: {
+      url: body.url,
+    },
+  })
+
   return NextResponse.json(
     {
       message: responseMessage.success.put,
-      data: {},
+      data: responseData,
     },
     { status: 201 },
   )
