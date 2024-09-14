366cdebc77efd2ca4d2de8d29a583d3d77ce377b
minusno0708 <naoyao1014@gmail.com>
2024-09-14 17:16:52 +0900

    feat: フォロワー取得機能を実装

--- app/api/follows/to/[id]/route.ts
+++ app/api/follows/to/[id]/route.ts
@@ -1,11 +1,32 @@
+import { PrismaClient } from "@prisma/client"
+import type { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
 
 import { responseMessage } from "@/app/api/types/responseMessage"
 
-export function GET() {
+const prisma = new PrismaClient()
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const userId: number = Number(params.id)
+
+  const userList = await prisma.follow.findMany({
+    where: {
+      toUserId: userId,
+    },
+    select: {
+      fromUserId: true,
+    },
+  })
+
+  const userIdList = userList.map((user) => user.fromUserId)
+
   return NextResponse.json(
     {
       message: responseMessage.success.get,
+      userIdList: userIdList,
     },
     { status: 200 },
   )
