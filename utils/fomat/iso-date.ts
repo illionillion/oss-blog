c62900c03b25f4b348c4d6e670ab3560facc5c0e
taku10101 <taku1011ht@gmail.com>
2024-09-15 21:20:31 +0900

    fix: 関数型修正

--- utils/fomat/iso-date.ts
+++ utils/fomat/iso-date.ts
@@ -1,5 +1,5 @@
-export const formatIsoDate = (date: Date) => {
+export const formatIsoDate = (date: string) => {
   const isoDate = new Date(date).toISOString()
   const strDate = isoDate.split("T")[0] // 2024-08-23形式に変換
-  return strDate
+  return strDate.toString()
 }
