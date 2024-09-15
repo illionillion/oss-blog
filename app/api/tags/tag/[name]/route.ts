f789aff97fcc68fe43e2d4e73ec38433484fcf77
minusno0708 <naoyao1014@gmail.com>
2024-09-15 12:44:58 +0900

    feat: タグの更新機能を実装

--- app/api/tags/tag/[name]/route.ts
+++ app/api/tags/tag/[name]/route.ts
@@ -33,3 +33,58 @@ export async function GET(
     { status: 200 },
   )
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { name: string } },
+) {
+  const name: string = params.name
+
+  let body
+
+  try {
+    body = await request.json()
+  } catch (error) {
+    // リクエストボディが存在しない場合にエラーを返す
+    console.log(error)
+    return NextResponse.json(
+      { message: responseMessage.error.invalidRequest },
+      { status: 400 },
+    )
+  }
+
+  const tag = await prisma.tag.findFirst({
+    where: {
+      name: name,
+    },
+  })
+
+  if (!tag) {
+    return NextResponse.json(
+      { message: responseMessage.error.notFound },
+      { status: 404 },
+    )
+  }
+
+  const updatedName = body.name !== undefined ? body.name : tag.name
+
+  const updatedIconUrl = body.iconUrl !== undefined ? body.iconUrl : tag.iconUrl
+
+  const responseData = await prisma.tag.update({
+    where: {
+      id: tag.id,
+    },
+    data: {
+      name: updatedName,
+      iconUrl: updatedIconUrl,
+    },
+  })
+
+  return NextResponse.json(
+    {
+      message: responseMessage.success.put,
+      data: responseData,
+    },
+    { status: 201 },
+  )
+}
