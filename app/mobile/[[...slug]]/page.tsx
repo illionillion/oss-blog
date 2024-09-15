4154fc485b1744a14e9a8b12171d2aaf1c19a1b7
SEKI-YUTA <zard.since1991@gmail.com>
2024-09-15 11:24:53 +0900

    feat: 記事を追加したカテゴリのpage.tsxを追加

---
+++ app/mobile/[[...slug]]/page.tsx
@@ -0,0 +1,41 @@
+import { Center, Heading, Text, VStack } from "@yamada-ui/react"
+import React from "react"
+import { Layout } from "@/components/layouts"
+import { CustomMarkdown } from "@/components/md/custom-markdown"
+import {
+  generateArticleMetadata,
+  generateStaticArticleParams,
+  getStaticArticleContent,
+} from "@/utils/next"
+
+interface Props {
+  params: { slug?: string[] }
+}
+
+export const dynamicParams = false
+
+export const generateMetadata = generateArticleMetadata("mobile")
+
+export const generateStaticParams = generateStaticArticleParams("mobile")
+
+const Page = async ({ params }: Props) => {
+  const { slug } = params
+
+  if (!slug) {
+    return <Text>Home page or default content</Text>
+  }
+
+  const { content, metadata } = await getStaticArticleContent("mobile")(slug)
+
+  return (
+    <Layout>
+      <Center as={VStack}>
+        <Heading>{metadata?.title}</Heading>
+        <Text>{metadata?.description}</Text>
+        <CustomMarkdown content={content} />
+      </Center>
+    </Layout>
+  )
+}
+
+export default Page
