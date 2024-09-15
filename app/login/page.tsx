79a92f3f0bb5f11155e71e14a24e8bd6338e50b9
taku10101 <taku1011ht@gmail.com>
2024-09-15 12:22:51 +0900

    fix: sign in状態によるボタンの切り替え

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,12 @@
 import React from "react"
-import { SignInButton } from "../../components/authButton"
+import { SignInButton, SignOutButton } from "../../components/authButton"
 import { auth } from "../../utils/auth/auth"
 
 const Page = async () => {
   const session = await auth()
   console.log(session)
   console.log(session?.user)
-  return (
-    <div>
-      <SignInButton />
-    </div>
-  )
+  return <div>{session?.user ? <SignOutButton /> : <SignInButton />}</div>
 }
 
 export default Page
