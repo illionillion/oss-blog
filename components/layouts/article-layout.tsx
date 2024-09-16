e814cae58387df186957c0a906a0abc5fd4a6b05
taku10101 <taku1011ht@gmail.com>
2024-09-16 10:06:05 +0900

    feat: 繋ぎ合わせ

--- components/layouts/article-layout.tsx
+++ components/layouts/article-layout.tsx
@@ -20,6 +20,7 @@ import type { FC } from "react"
 import { Contributor } from "../data-display/contributors"
 import { CustomMarkdown } from "../md/custom-markdown"
 import { TableOfContents } from "../navigation/table-of-contents"
+import { ArticleButtons } from "../forms/article-buttons"
 
 export interface ArticleLayoutProps {
   metadata: ArticleMetadata | undefined
@@ -32,63 +33,7 @@ export const ArticleLayout: FC<ArticleLayoutProps> = ({
 }) => {
   return (
     <HStack maxW="9xl" w="full" alignItems="start" m="auto">
-      <VStack
-        maxW="2xs"
-        w="full"
-        alignItems="center"
-        position="sticky"
-        top="4rem"
-        py="lg"
-        display={{ base: "flex", md: "none" }}
-      >
-        <IconButton
-          variant="ghost"
-          colorScheme="primary"
-          fontSize="3xl"
-          boxSize="10"
-          as={Link}
-          href="#"
-          icon={<ThumbsUpIcon />}
-        />
-        <IconButton
-          variant="ghost"
-          colorScheme="primary"
-          fontSize="3xl"
-          boxSize="10"
-          as={Link}
-          href="#"
-          icon={<BookMarkedIcon />}
-        />
-        <IconButton
-          variant="ghost"
-          colorScheme="primary"
-          fontSize="3xl"
-          boxSize="10"
-          as={Link}
-          target="_blank"
-          href="https://github.com/illionillion/oss-blog/issues/new"
-          icon={<InfoIcon />}
-        />
-        <IconButton
-          variant="ghost"
-          colorScheme="primary"
-          fontSize="3xl"
-          boxSize="10"
-          as={Link}
-          target="_blank"
-          href={`https://github.com/illionillion/oss-blog/tree/main/contents/${metadata?.slug}`}
-          icon={<CodeIcon />}
-        />
-        <IconButton
-          variant="ghost"
-          colorScheme="primary"
-          fontSize="3xl"
-          boxSize="10"
-          as={Link}
-          href="#"
-          icon={<XIcon />}
-        />
-      </VStack>
+      <ArticleButtons metadata={metadata} />
       <VStack gap="md" flexGrow={1}>
         <VStack>
           <Heading>{metadata?.title}</Heading>
