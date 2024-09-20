fc8116b4d44750e1de9d7ca871fe1a62bbee60c5
taku10101 <taku1011ht@gmail.com>
2024-09-20 12:23:21 +0900

    Revert "feat: backendの機能を統合"
    This reverts commit 55d4e8483173655d10ffa4c679ba328893db518b.

--- app/api/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  searchEndpoints: {
-    qiita: "https://qiita.com/api/v2/items",
-    zenn: "https://zenn.dev/api/articles",
-    hatena: "https://b.hatena.ne.jp/entrylist/json",
-  },
-  perplexityApiKey: process.env.PERPLEXITY_API_KEY,
-}
